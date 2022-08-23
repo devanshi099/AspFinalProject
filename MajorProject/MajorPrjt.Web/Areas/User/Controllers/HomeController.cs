97bd63e3cf3ea25a5cfd1248993cbe51e43d4797
Devanshi <agrawaldeva099@gmail.com>
2022-08-23 17:52:53 +0530

    Added UserForumViewModel

---
+++ MajorProject/MajorPrjt.Web/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MajorPrjt.Web.Data;
+using MajorPrjt.Web.Areas.User.ViewModels;
+
+namespace MajorPrjt.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Populate the data for the drop-down select list
+            List<SelectListItem> categories = new List<SelectListItem>();
+            categories.Add(new SelectListItem { Selected = true, Value = "", Text = "-- select a category --" });
+            categories.AddRange(new SelectList(_context.Categories, "CategoryId", "CategoryName"));
+            ViewData["CategoryId"] = categories.ToArray();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index([Bind("CategoryId")] UserForumViewModel model)
+        {
+            // Retrieve the Menu Items for the selected category
+            var items = _context.Topics.Where(m => m.CategoryId == model.CategoryId);
+
+            // Populate the data into the viewmodel object
+            model.Topics = items.ToList();
+
+            // Populate the data for the drop-down select list
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName");
+
+            // Display the View
+            return View("Index", model);
+        }
+
+        
+        public IActionResult CustomerView()
+        {
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MajorProject/MajorPrjt.Web/Areas/User/ViewModels/UserForumViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MajorPrjt.Web.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MajorPrjt.Web.Areas.User.ViewModels
+{
+    public class UserForumViewModel
+    {
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+
+        public ICollection<Topic> Topics { get; set; }
+    }
+}
